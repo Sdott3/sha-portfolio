a51ca5e0649507d0bfe2b238863c6c93f5eb7279
sdott3 <sdotthomas347@gmail.com>
2022-09-25 08:32:49 -0400

    commented stuff out so it can render

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 
 import React from 'react';
-import Nav from './components/Nav/index.js';
+// import Nav from './components/Nav/index.js';
 import About from './components/About/index.js';
-import Contact from './components/Contact/index.js';
-import Portfolio from './components/Portfolio/index.js';
-import Resume from './components/Resume/index.js';
-import Footer from './components/Footer/index.js';
+// import Contact from './components/Contact/index.js';
+// import Portfolio from './components/Portfolio/index.js';
+// import Resume from './components/Resume/index.js';
+// import Footer from './components/Footer/index.js';
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Nav />
+      {/* <Nav /> */}
       <About />
-      <Contact />
+      {/* <Contact />
       <Portfolio />
       <Resume />
-      <Footer />
+      <Footer /> */}
      
     </main>
   );
