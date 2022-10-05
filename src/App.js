b3834964d31aa0396a40a47c492ebb14cdf765b7
sdott3 <sdotthomas347@gmail.com>
2022-10-05 13:34:47 -0400

    Nav bar updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 
 import React from 'react';
-// import Nav from './components/Nav/index.js';
+import Nav from './components/Nav/index.js';
 import About from './components/About/index.js';
-import Contact from './components/Contact/index.js';
+// import Contact from './components/Contact/index.js';
 // import Portfolio from './components/Portfolio/index.js';
 // import Resume from './components/Resume/index.js';
 // import Footer from './components/Footer/index.js';
 
 export default function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
-      {/* <Nav /> */}
+    <main className="text-white-400 bg-blue-300 body-font">
+      <Nav />
       <About />
-      <Contact />
+      {/* <Contact /> */}
       {/* <Portfolio />
       <Resume />
       <Footer /> *} */}
