b3f3e9aae9946d7bc176b97869ad30ad9a1c118d
sdott3 <sdotthomas347@gmail.com>
2022-10-06 21:01:51 -0400

    updated About me

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,18 +4,10 @@ export default function Nav() {
     return (
         <header className="bg-blue-300 md:sticky ">
             <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-                <a href="#projects" className="mr-5 hover:text-white">
-                    Projects
-                </a>
-                <a href="#skills" className="mr-5 hover:text-white">
-                    Skills
-                </a>
-                <a href="#contact" className="mr-5 hover:text-white">
-                    Contact Me
-                </a>
-                <a href="#resume" className="mr-5 hover:text-white">
-                    Resume
-                </a>
+                
+                
+                    
+                    
             </nav>
         </header>
     );
